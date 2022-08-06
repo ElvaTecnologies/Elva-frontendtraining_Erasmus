49726b459d3ecf22aefa8a97864665becdb54c7c
Erasmus Mensah <79203686+Erasmus001@users.noreply.github.com>
2022-08-05 20:46:35 -0700

    complete project

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={classes.footer}>
+      <h4>Footer</h4>
+    </div>
+  );
+};
+
+export default Footer;
